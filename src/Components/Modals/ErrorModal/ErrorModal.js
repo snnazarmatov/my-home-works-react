ef8108c09ec205ec0f07f9eda9fcebfa75db3b29
Esen Nazarmatov <snnazarmatov@gmail.com>
2021-11-20 04:09:45 +0600

    added some comment

--- src/Components/Modals/ErrorModal/ErrorModal.js
+++ src/Components/Modals/ErrorModal/ErrorModal.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import ModalOverlay from './ModalOverLay';
+import ModalOverlay from '../ErrorModal/ModalOverlay';
 
+//ErrorModal ды каалаган жерде чакыра беребиз Portal аркылуу аны рендер кылабыз public тин ичинде 2 дивтин ичине(<div id="backdrop-root"></div>)
 const ErrorModal = props => {
     return (
     <React.Fragment>
         {ReactDOM.createPortal(
+//Portall 2 аргументе алат.1-Backdrop component который мы рендерим. 2-аргумент (backdrop-root) ту getElementById кылабыз.backdrop div тин ичинде(парент катары root тун жанында) озгочо озунчо рендер болуп жатат анкени ал бардык компонентерден жогору туруш керек.анан пропстарын туура бериш керек аны биз ErrorModal га беребиз form турган жерге.
             <Backdrop onConfirm={props.onConfirm}/>,
             document.getElementById('backdrop-root')
         )} 
         {ReactDOM.createPortal(
                 <ModalOverlay
                 title={props.title} 
+    //props ErrorModal дан келет.
                 message={props.message} 
                 onConfirm={props.onConfirm}
                 />,
