48e6f10a3433d548b2603bd91e28ba48e1c821a6
Esen Nazarmatov <snnazarmatov@gmail.com>
2021-12-13 05:32:04 +0600

    to do

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Todos from './Components/Todos';
 
 
+//Акыркы сабактагы биз тузгон useInput аркылуу BasicForm.js компонентин инпутарын дагым обработка кылып чыккыла
+
 function App() {
   return (
     <div className="App">
