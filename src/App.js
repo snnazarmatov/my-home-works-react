d4ca317bad5000b7e28c962ac95a26bed90e9fcf
Esen Nazarmatov <snnazarmatov@gmail.com>
2021-12-17 22:03:15 +0600

    to do

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Todos from './Components/Todos';
 
+// Сакабактагы аткарган кодду кайрадан жазып чыккыла бирок жонокой редакс менен 
+// Redux toolkit бар болсо тапышрма текшерилбейт
 
 function App() {
   return (
