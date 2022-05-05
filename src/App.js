4c57bc940c235b3da470f03fa70a33dd74d53d48
Esen Nazarmatov <snnazarmatov@gmail.com>
2022-05-05 09:03:03 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import Todos from './Components/Todos';
+import React from 'react';
 
 // Сакабактагы аткарган кодду кайрадан жазып чыккыла бирок жонокой редакс менен 
 // Redux toolkit бар болсо тапышрма текшерилбейт
 
 function App() {
+  const [state,setState] = React.useState('')
+  
   return (
-    <div className="App">
-      <Todos/>
-    </div>
+    <form>
+    <input type='text' {style='backgroundColor:red'} id='input1' onChange={(e) => setState(e.target.value)} value={state}/>
+      <input type='text' style='backgroundColor:blue;' id='input2' onChange={(e) => setState(e.target.value)} value={state}/>
+      <input type='text' onChange={(e) => setState(e.target.value)} value={state}/>
+      <button></button>
+    </form>
   );
+
+
 }
 
 export default App;
